63872f10ab87fda55892857434857638dbf02fa2
Asha Rani <asharani97@hotmail.com>
2024-01-19 14:51:30 +0530

    Update pair.py

--- data/pair.py
+++ data/pair.py
@@ -2,32 +2,21 @@ import torch
 import numpy as np
 import numpy
 
-def arrange(v1, v2, l1,l2):
+def arrange(v1, v2, l1, l2):
     n = len(v1)
-    m = int(n/2)
+    m = n // 2
+    video1_1, video1_2 = v1.split(m)    
     if l1.tolist() != l2.tolist():
-        video1_1 = v1[0:m]
-        video1_2 = v1[m:]
-        video2_1 = v2[m:]
-        video2_2 = v2[0:m]
-        return video1_1,video1_2, video2_1, video2_2
+        video2_1, video2_2 = v2.split(m)
     else:
-        video1_1 = v1[0:m]
-        video1_2 = v1[m:]
-        video2_1 = v2[0:m]
-        video2_2 = v2[m:]
-        return video1_1, video1_2, video2_1, video2_2
+        video2_1, video2_2 = v2[m:], v2[:m]
+    return video1_1, video1_2, video2_1, video2_2
 
-def make_pair(p1,p2):
-    new_vid1=[]
-    new_vid2 =[]
-    n =len(p1)
-    for i in range(n):
-        for j in range(n):
-            new_vid1.append(p1[i])
-            new_vid2.append(p2[j])
-   
-    return torch.stack(new_vid1, dim=0), torch.stack(new_vid2,dim=0)
+
+def make_pair(p1, p2):
+    new_vid1 = torch.repeat(p1.unsqueeze(1), 1, len(p2)).view(-1, len(p1))
+    new_vid2 = p2.repeat(len(p1))
+    return new_vid1, new_vid2
 
 def convert(inp):
     p = numpy.asarray(inp,dtype=object)
